a919e1d67d1bc2296fc5730cab30f328a0aaf6fe
Renato Oliveira <renato@labcodes.com.br>
2016-10-26 00:58:20 -0300

    Add Lead model.

--- core/models.py
+++ core/models.py
@@ -1,4 +1,15 @@
+# coding: utf-8
+
 from __future__ import unicode_literals
 
 from django.db import models
 
+
+class Lead(models.Model):
+
+    name = models.CharField('Nome', max_length=256)
+    email = models.EmailField('E-mail')
+    short_description = models.TextField(u'Descrição')
+
+    def __unicode__(self):
+        return self.name + ' | ' + self.email
