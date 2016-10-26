b83d2a3678f8ddca8517c71206be106bbfa3e750
Renato Oliveira <renato@labcodes.com.br>
2016-10-26 01:15:51 -0300

    Remove static reference.

--- django_example/settings.py
+++ django_example/settings.py
@@ -123,8 +123,4 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
